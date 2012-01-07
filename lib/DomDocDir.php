bee849d1075a34c90afb7529240ea90f77288e55
qur2 <aurelien.scoubeau@gmail.com>
2012-01-08 00:18:47 +0100

    Improved DomDocDir class :  - Added comments.  - Removed useless method.

--- lib/DomDocDir.php
+++ lib/DomDocDir.php
@@ -1,12 +1,29 @@
 <?php
+/**
+ * Represents a directory used by a DomDoc.
+ * It's a directory meant to contain a single HTML file and assets in subdirectories.
+ * The assets have to be included using relative paths.
+ * @todo handle multiple HTML files in the same directory.
+ */
 class DomDocDir {
+	/**
+	 * The resource path.
+	 */
 	private $path;
 
 
+	/**
+	 * Constructor.
+	 */
 	public function __construct($path = false) {
 		$this->setPath($path);
 	}
 
+	/**
+	 * Setter for the $path attribute. It also set a $name attribute,
+	 * which the $path base name.
+	 * @param string $path The path of the directory.
+	 */
 	private function setPath($path) {
 		$this->path = rtrim($path, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
 		$this->name = false;
@@ -15,18 +32,25 @@ class DomDocDir {
 		}
 	}
 
+	/**
+	 * Getter for the $path attribute.
+	 */
 	public function getPath() {
 		return $this->path;
 	}
 
+	/**
+	 * Tells if the directory exists.
+	 * @return boolean True if the directory exists, false otherwise.
+	 */
 	public function exists() {
 		return false !== $this->path && is_dir($this->path);
 	}
-	
-	public function isRoot() {
-		return $this->exists() && glob($this->path . $this->name . '.*');
-	}
 
+	/**
+	 * Consolidate the directory by checking that the content is not too deep.
+	 * It may happen when unzipping a complete directory.
+	 */
 	private function moveBackUp() {
 		$tooFar = $this->path . '/' . $this->name;
 		if (is_dir($tooFar)) {
@@ -36,14 +60,24 @@ class DomDocDir {
 		}
 	}
 
+	/**
+	 * Checks if a file has the same name (without extension) that the directory base name.
+	 * If not, it searches for a file of a given extension and renames it.
+	 * @param string $ext The file extension to use for renaming.
+	 * @return boolean A boolean telling if the renaming was successful or not.
+	 * @todo Delete this method to handle multiple HTML files as it will become useless.
+	 */
 	private function makeRoot($ext = 'html') {
-		if ($this->isRoot())
-			return;
 		$file = glob($this->path . '*.' . $ext);
 		$file = reset($file);
 		return rename($file, $this->path . $this->name . '.' . $ext);
 	}
 
+	/**
+	 * Extracts a zip to a given destination and consolidates the directory.
+	 * @param string $location The zip location.
+	 * @param string $destination The directory path where the zip is extracted.
+	 */
 	public function buildFromZip($location, $destination) {
 		$zip = new ZipArchive;
 		if (true !== $zip->open($location)) {
